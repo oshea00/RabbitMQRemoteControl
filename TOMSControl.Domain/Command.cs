18b816621f2d6884f0987768798e04cb962acb77
Mike OShea <mike@limpidfox.com>
2012-04-12 16:26:49 -0700

    Change tag name and use environment getroute in job

--- TOMSControl.Domain/Command.cs
+++ TOMSControl.Domain/Command.cs
@@ -9,23 +9,23 @@ namespace TOMSControl.Domain
     {
         public string RoutingKey { get; set; }
         public string Name { get; set; }
-        public string Tag { get; set; }
+        public string CommandQueue { get; set; }
         public string ExecuteFile { get; set; }
         public string WorkingDirectory { get; set; }
         public string Arguments { get; set; }
         public int    Ticket { get; set; }
 
-        public Command(string name, string tag)
+        public Command(string name, string commandqueue)
         {
             if (!String.IsNullOrWhiteSpace(name))
                 Name = name;
             else
                 throw new Exception("Name must not be empty");
 
-            if (!String.IsNullOrWhiteSpace(tag) && !tag.Contains(" "))
-                Tag = tag;
+            if (!String.IsNullOrWhiteSpace(commandqueue) && !commandqueue.Contains(" "))
+                CommandQueue = commandqueue;
             else
-                throw new Exception("Tag must not be empty or contain whitespace");
+                throw new Exception("Command queue must not be empty or contain whitespace");
         }
 
         public CommandMessage CommandMessage()
